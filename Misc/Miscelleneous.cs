ec4cae6f8565ed82ec6030fa6dc03c340eafe6a7
Abdullahi Abass <abassabdullahi4@gmail.com>
2018-11-26 20:49:14 +0100

    added simple html fetch function

---
+++ Misc/Miscelleneous.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace culTAKU.Misc
+{
+    public static class Miscelleneous
+    {
+        public static string getHtml(string url)
+        {
+            string html = string.Empty;
+            
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Uri.EscapeUriString(url));
+            HttpWebResponse response = null;
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Status);
+            }
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+
+            html = reader.ReadToEnd();
+            return html;
+        }
+    }
+}
